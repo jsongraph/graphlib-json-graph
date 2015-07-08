4b57b205dd81e50f660641f03a014aeed9ff4f9a
RangerMauve <RangerMauve@htomail.com>
2015-07-07 21:43:52 -0400

    Created test case descriptions

---
+++ test/index.js
@@ -0,0 +1,13 @@
+var toGraph = require("../");
+
+describe("toGraph()", function() {
+	it.skip("should be able to parse a single graph", function() {});
+	it.skip("should be able to parse multiple graphs", function() {});
+	it.skip("should throw an error if there are no graphs", function() {});
+	it.skip("should support graphs without nodes", function() {});
+	it.skip("should support graphs without edges", function() {});
+	it.skip("should respect the directed property of a graph", function() {});
+	it.skip("should make a graph directed if any of the edges are directed", function() {});
+	it.skip("should be a multigraph", function() {});
+	it.skip("should use the id for distinguishing edges between the same nodes", function() {});
+});
